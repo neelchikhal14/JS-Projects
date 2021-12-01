e9eab4ecc5ebd55fb257d8b2bc6d1518f1a04cf8
neelchikhal14 <neelchikhal14@gmail.com>
2021-12-01 03:05:11 +0000

    completed slider project

---
+++ 03 - Custom Range Slider/script.js
@@ -0,0 +1,26 @@
+// Github Link
+// https://github.com/bradtraversy/50projects50days
+const range = document.getElementById('range');
+
+range.addEventListener('input', (e) => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+
+  const rangeWidth = getComputedStyle(e.target).getPropertyValue('width');
+  // console.log(rangeWidth);
+  const labelWidth = getComputedStyle(label).getPropertyValue('width');
+  // console.log(labelWidth);
+
+  const numWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
+  const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2);
+  console.log(numWidth, numLabelWidth);
+
+  const max = +e.target.max;
+  const min = +e.target.min;
+
+  const left = value * (numWidth / max) - numLabelWidth / 2;
+
+  label.style.left = `${left}px`;
+
+  label.innerHTML = value;
+});
